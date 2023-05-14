078878ca0dcdb84f8bc7f779315a0a2def0ccbe6
Alex Widener <awidener3@gmail.com>
2023-05-14 00:34:47 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
 	base: '/dm-deck-notion/', // github pages repo (https://<USERNAME>.github.io/<REPO>/)
+	plugins: [react()],
 });
