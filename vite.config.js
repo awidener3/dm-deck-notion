1a2d3ee6cc7a58f7d7c7b4341076b09f1b6e2f82
Alex Widener <awidener3@gmail.com>
2023-05-20 12:11:30 -0400

    remove references to original basename

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/dm-deck-notion/', // github pages repo (https://<USERNAME>.github.io/<REPO>/)
 	plugins: [react()],
 });
